f6ec9f7882eb01e3ce3828082d5eba9001cdc392
Alejandro Serna <aserna.villa@udea.edu.co>
2026-09-25 12:13:15 -0500

    Correcion algoritmo RLE

--- Practica3_info2/RLE.cpp
+++ Practica3_info2/RLE.cpp
@@ -24,7 +24,7 @@ string RLEptr(const char* cadena){
 
 
 string iniciarRLE(){ //funcion que invoca algoritmo RLE e imprime la compresion
-    char inicial[] = "8888888888";
+    char inicial[] = "AAAAAAAAAAB";
     const char* cadena = inicial;
 
     return RLEptr(cadena);
@@ -35,16 +35,31 @@ string recuperarInicial(string resul){
     int n = 0;
 
     for(int i = 0;i < resul.length();i+=2){   //recorrer la compresión de derecha a izquierda
-        //char caracter_actual = resul[i];
-            int num_rep = resul[i] - '0'; //convertimos a int para iterar
 
-            char letra = resul[(n+1)];
-            n+=2;
+        int num_rep = resul[i] - '0'; //convertimos a int para iterar
+        if(resul[i + 1] >= '0' && resul[i+1] <= '9'){
+            num_rep = num_rep * 10 + (resul[i+1] - '0');
+            char letra = resul[(n+2)];
+            i+=1;
 
             for(int j = 0; j < num_rep;j++){
 
                 original += letra;
             }
+            n+=3;
+        }
+
+            else{
+                char letra = resul[(n+1)];
+                for(int j = 0; j < num_rep;j++){
+
+                    original += letra;
+
+                }
+                n+=2;
+            }
+
+
 //fucnion de verificacion y verificar cuando la entrada es mayor a 9 dígitos
 }
     return original;
